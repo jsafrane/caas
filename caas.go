aa6057b84dc4e139cf78d9f259803943c8d616cf
Jan Safranek <jsafrane@redhat.com>
2019-04-16 20:40:21 +0200

    Resolve all cassandra addresses before connecting
    + improve error handling and logging

--- caas.go
+++ caas.go
@@ -4,8 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"os"
+	"net"
 	"net/http"
+	"os"
 
 	"github.com/gocql/gocql"
 	"github.com/gorilla/mux"
@@ -22,38 +23,45 @@ func bump(name string) error {
 	return session.Query(`UPDATE counter SET value=value+1 WHERE name = ?`, name).Exec()
 }
 
-func get(name string) (Counter, int) {
+func get(name string) (Counter, error) {
 	var counter Counter
-	var found bool
 	m := map[string]interface{}{}
-	query := "SELECT name, value FROM counter WHERE name=? LIMIT 1"
-	iterable := session.Query(query, name).Consistency(gocql.One).Iter()
-	for iterable.MapScan(m) {
-		found = true
-		counter = Counter{
-			Name:  m["name"].(string),
-			Value: m["value"].(int64),
-		}
+	cql := "SELECT name, value FROM counter WHERE name=? LIMIT 1"
+	query := session.Query(cql, name).Consistency(gocql.One)
+	if err := query.MapScan(m); err != nil {
+		return Counter{}, err
 	}
-	if !found {
-		return Counter{}, http.StatusNotFound
+	counter = Counter{
+		Name:  m["name"].(string),
+		Value: m["value"].(int64),
 	}
-	return counter, http.StatusOK
+	return counter, nil
 }
 
 func Get(w http.ResponseWriter, r *http.Request) {
-
 	vars := mux.Vars(r)
 	name := vars["counter_name"]
+	log.Printf("Processing request for counter %s", name)
 
 	err := bump(name)
 	if err != nil {
+		log.Printf("Error bumping counter %s: %s", name, err)
 		w.WriteHeader(http.StatusInternalServerError)
+		errJson := fmt.Sprintf("{\"error\": \"%s\"}\n", err)
+		w.Write([]byte(errJson))
+		return
+	}
+	counter, err := get(name)
+	if err != nil {
+		log.Printf("Error loading counter %s: %v", name, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		errJson := fmt.Sprintf("{\"error\": \"%s\"}\n", err)
+		w.Write([]byte(errJson))
 		return
 	}
 
-	counter, errCode := get(name)
-	w.WriteHeader(errCode)
+	log.Printf("Counter %s bumped to %+v", name, counter)
+	w.WriteHeader(http.StatusOK)
 	_ = json.NewEncoder(w).Encode(counter)
 }
 
@@ -62,9 +70,18 @@ func connect() error {
 	if addr == "" {
 		return fmt.Errorf("CASSANDRA_ADDRESS must be set")
 	}
-	cluster := gocql.NewCluster(addr)
+
+	// We need to resolve all IPs of cassandra server and connect to them
+	ips, err := net.LookupHost(addr)
+	if err != nil {
+		return fmt.Errorf("cannot resolve %s: %s", addr, err)
+	}
+
+	log.Printf("Resolved cassandra address %s to %+v", addr, ips)
+
+	cluster := gocql.NewCluster(ips...)
 	cluster.Keyspace = "caas"
-	var err error
+
 	session, err = cluster.CreateSession()
 	return err
 }
